3ec1442d499ce997e2f876dfc5e4bcbefff84217
PratikMahobiya <pratikmahobiya18@gmail.com>
2024-10-06 05:05:56 +0530

    Exit Api Fix

--- account/views.py
+++ account/views.py
@@ -1,3 +1,4 @@
+import json
 import threading
 from zoneinfo import ZoneInfo
 from datetime import datetime
@@ -12,8 +13,9 @@ def AccountExitApi(request):
     now = datetime.now(tz=ZoneInfo("Asia/Kolkata"))
     try:
         print(f"MoneyBall: Account Exit Api : Started {now.strftime('%d-%b-%Y %H:%M:%S')}")
+        data = json.loads(request.body)
         # Streaming threads for Open Positions
-        exit_thread = threading.Thread(name=f"API-Exit-{now.strftime('%d-%b-%Y %H:%M:%S')}", target=AccountExitAction, args=(request.data), daemon=True)
+        exit_thread = threading.Thread(name=f"API-Exit-{now.strftime('%d-%b-%Y %H:%M:%S')}", target=AccountExitAction, args=(data), daemon=True)
         exit_thread.start()
 
         print(f'MoneyBall: Account Exit Api : Execution Time(hh:mm:ss) : {(datetime.now(tz=ZoneInfo("Asia/Kolkata")) - now)}')
