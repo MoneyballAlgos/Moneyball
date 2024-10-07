addc4690ed72dcf3ff521c725de63f83aaf2bea4
PratikMahobiya <pratikmahobiya18@gmail.com>
2024-10-07 09:43:47 +0530

    Socket Stream API FIX

--- helper/trade_action.py
+++ helper/trade_action.py
@@ -62,7 +62,7 @@ def Price_Action_Trade(data, new_entry):
                     "exchangeType": index+1,
                     "tokens": i
                 })
-        url = f"{SOCKET_STREAM_URL_DOMAIN}/api/trade/socket-stream/"
+        url = f"{SOCKET_STREAM_URL_DOMAIN}/api/system_conf/socket-stream/"
         data_json = {
             "subscribe_list": subscribe_list,
             "correlation_id": correlation_id,
