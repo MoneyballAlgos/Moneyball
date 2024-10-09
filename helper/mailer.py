33e922cb962f53fc0b21f8e8c54bb90064a3506e
PratikMahobiya <pratikmahobiya18@gmail.com>
2024-10-09 16:11:33 +0530

    Mailer Setup

---
+++ helper/mailer.py
@@ -0,0 +1,45 @@
+import ssl
+import smtplib
+from datetime import datetime
+from email.utils import formataddr
+from email.message import EmailMessage
+from moneyball.settings import SMTP_SENDER_EMAIL, SMTP_SENDER_NAME, SMTP_SENDER_PASSWORD
+
+
+def smtp_send_email(msg_html, subject, recipient, cc_email=None, bcc_email=None, file_name=None, payload=None):
+    try:
+        msg = EmailMessage()
+        msg['Subject'] = subject
+        msg['From'] = formataddr(SMTP_SENDER_NAME, SMTP_SENDER_EMAIL)
+        msg['To'] = recipient
+        if cc_email is not None and len(cc_email) > 0:
+            msg ['CC'] = cc_email
+        if bcc_email is not None and len(bcc_email) > 0:
+            msg ['BCC'] = bcc_email
+            
+        args ={
+            "from_email": SMTP_SENDER_EMAIL,
+            "email_sent_time": str(datetime.now()),
+            "subject": subject
+        }
+
+        if payload is not None: 
+            for p_key, p_value in payload. items ():
+                args [p_key] = p_value
+        
+        msg[' custom_args'] = str(args)
+        msg.set_content(msg_html, subtype='html')
+        context = ssl.create_default_context()
+        with smtplib.SMTP('smtp.gmail.com', 465) as smtp:
+            smtp.ehlo() # Can be omitted, called implicitly by the method if required
+            smtp.starttls(context=context)
+            smtp.ehlo() # Can be omitted, called implicitly by the method if required
+            smtp.login(SMTP_SENDER_EMAIL, SMTP_SENDER_PASSWORD) 
+            smtp.send_message(msg)
+            smtp.quit()
+            status = True
+    except Exception as e:
+        print(f"SMTP send email Error: {subject} : {str(e)}")
+        print(f"SMTP: Additional Details: TO_EMAIL: {recipient} : FROM_EMAIL: {SMTP_SENDER_NAME}")
+        status = False
+    return status
--- moneyball/settings.py
+++ moneyball/settings.py
@@ -152,6 +152,10 @@ BROKER_USER_ID = os.getenv("BROKER_USER_ID", 'User-Id Required')
 BROKER_API_KEY = os.getenv("BROKER_API_KEY", 'Api Key Required')
 BROKER_TOTP_KEY = os.getenv("BROKER_TOTP_KEY", 'TOTP Key required')
 
+SMTP_SENDER_NAME = os.getenv("SMTP_SENDER_NAME", 'SMTP Sender name required')
+SMTP_SENDER_EMAIL = os.getenv("SMTP_SENDER_EMAIL", 'SMTP Sender email required')
+SMTP_SENDER_PASSWORD = os.getenv("SMTP_SENDER_PASSWORD", 'SMTP Sender password required')
+
 
 # Global Variable
 global broker_connection, account_connections, entry_holder
