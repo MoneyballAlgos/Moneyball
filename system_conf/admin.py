5df0b941de54d2cbe52f1c6485bf0dfd717c99d5
PratikMahobiya <pratikmahobiya18@gmail.com>
2024-10-23 16:43:54 +0530

    Requirement updated

--- system_conf/admin.py
+++ system_conf/admin.py
@@ -11,12 +11,12 @@ class ConfigurationAdmin(admin.ModelAdmin):
 
 @admin.register(Symbol)
 class SymbolAdmin(admin.ModelAdmin):
-    list_display = ('fno', 'name', 'percentchange_', 'volume', 'valuechange', 'ltp', 'weekhigh52', 'weeklow52', 'strike', 'exchange', 'symbol', 'product', 'expiry', 'token', 'oi', 'lot', 'pivot', 'r1', 'r2', 'r3', 's1', 's2', 's3', 'is_active')
+    list_display = ('fno', 'name', 'percentchange_', 'ltp', 'weekhigh52', 'weeklow52', 'nifty50', 'nifty100', 'nifty200', 'midcpnifty50', 'midcpnifty100', 'midcpnifty150', 'smallcpnifty50', 'smallcpnifty100', 'smallcpnifty250', 'exchange', 'pivot', 'r1', 'r2', 'r3', 's1', 's2', 's3', 'product', 'expiry', 'strike', 'symbol', 'token', 'volume', 'valuechange', 'oi', 'lot', 'is_active')
     list_filter = ('product', 'exchange', 'fno', 'nifty50', 'nifty100', 'nifty200', 'midcpnifty50', 'midcpnifty100', 'midcpnifty150', 'smallcpnifty50', 'smallcpnifty100', 'smallcpnifty250')
     search_fields = ['name', 'symbol', 'token']
 
     def get_ordering(self, request):
-        return ['-percentchange']
+        return ['-nifty200', '-nifty100', '-nifty50', '-midcpnifty150', '-midcpnifty100', '-midcpnifty50', '-smallcpnifty250', '-smallcpnifty100', '-smallcpnifty50', '-percentchange']
     
     def percentchange_(self, obj):
         return colour(obj.percentchange)
