daf4a92eda20aee11b4e3bd8fdc4e6337d013e66
PratikMahobiya <pratikmahobiya18@gmail.com>
2024-10-28 13:29:35 +0530

    Exception fix

--- helper/angel_function.py
+++ helper/angel_function.py
@@ -14,9 +14,9 @@ def historical_data(token, exchange, now, from_day, interval, product):
     global broker_connection, account_connections
     connection = None
     if product == 'future':
-        connection = account_connections.get('H188598')
-    elif product == 'equity':
         connection = account_connections.get('P567723')
+    elif product == 'equity':
+        connection = account_connections.get('H188598')
     else:
         connection = broker_connection
     data = connection.getCandleData(historicParam)
@@ -24,20 +24,20 @@ def historical_data(token, exchange, now, from_day, interval, product):
         raise Exception(f"Historical API error: {product} : {historicParam} : {data['errorcode']} : {data['message']}")
     else:
         data = data['data']
-    data.rename(columns={
-        0: 'date', 1: 'Open', 2: 'High', 3: 'Low', 4: 'Close', 5: 'Volume'}, inplace=True)
-    data.index.names = ['date']
-    data_frame = data
-    # Convert str timestamps to IST
-    ist_timezone = pytz.timezone('Asia/Kolkata')
-    data_frame['date'] = data_frame['date'].apply(lambda x: datetime.fromisoformat(x).astimezone(ist_timezone))
+        data.rename(columns={
+            0: 'date', 1: 'Open', 2: 'High', 3: 'Low', 4: 'Close', 5: 'Volume'}, inplace=True)
+        data.index.names = ['date']
+        data_frame = data
+        # Convert str timestamps to IST
+        ist_timezone = pytz.timezone('Asia/Kolkata')
+        data_frame['date'] = data_frame['date'].apply(lambda x: datetime.fromisoformat(x).astimezone(ist_timezone))
 
-    # Compare the minutes
-    if product == 'future':
-        if (data_frame['date'].iloc[-1].minute >= now.minute) and (data_frame['date'].iloc[-1].hour >= now.hour):
-            data_frame = data_frame.iloc[:-1]
-        else:
-            data_frame = data_frame
+        # Compare the minutes
+        if product == 'future':
+            if (data_frame['date'].iloc[-1].minute >= now.minute) and (data_frame['date'].iloc[-1].hour >= now.hour):
+                data_frame = data_frame.iloc[:-1]
+            else:
+                data_frame = data_frame
 
-    data_frame = data_frame.fillna(data_frame.mean())
-    return data_frame
+        data_frame = data_frame.fillna(data_frame.mean())
+        return data_frame
