7652813236bd8cf3314319ea1c4bf8dd57a41dea
PratikMahobiya <pratikmahobiya18@gmail.com>
2024-10-29 12:25:50 +0530

    Always alive

--- clock.py
+++ clock.py
@@ -9,11 +9,11 @@ def start():
     socket_setup(log_identifier='Restart')
 
     # Schedules job_function to be run on the Monday to Friday
-    sched.add_job(stay_awake, 'cron', day_of_week='mon-fri',
+    sched.add_job(stay_awake, 'cron',
                 second='*/40', timezone='Asia/Kolkata')
-    sched.add_job(BrokerConnection, 'cron', day_of_week='mon-fri',
+    sched.add_job(BrokerConnection,
                 hour='9', minute='11', timezone='Asia/Kolkata')
-    sched.add_job(socket_setup, 'cron', day_of_week='mon-fri',
+    sched.add_job(socket_setup,
                 hour='9', minute='12', timezone='Asia/Kolkata')
     # sched.add_job(stop_socket_setup, 'cron', day_of_week='mon-fri',
     #             hour='5', minute='5', timezone='Asia/Kolkata')
