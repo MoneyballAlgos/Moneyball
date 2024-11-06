409c983f6396b33efdf4e36771c449553239cb37
PratikMahobiya <pratikmahobiya18@gmail.com>
2024-11-06 15:51:31 +0530

    Fix

--- clock.py
+++ clock.py
@@ -10,7 +10,7 @@ def start():
 
     # Schedules job_function to be run on the Monday to Friday
     sched.add_job(stay_awake, 'cron',
-                second='*/50', timezone='Asia/Kolkata')
+                second='*/40', timezone='Asia/Kolkata')
     sched.add_job(BrokerConnection, 'cron',
                 hour='9', minute='11', timezone='Asia/Kolkata')
     sched.add_job(socket_setup, 'cron',
